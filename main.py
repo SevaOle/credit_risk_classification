0bae8183d0c0d661e7b6dee2790713ddd299a68a
SevaOle <98477078+SevaOle@users.noreply.github.com>
2026-03-12 05:10:50 -0400

    implemented random forest
    +removed non-borrower features as they leaked the target leading to 100% accuracy

--- main.py
+++ main.py
@@ -5,9 +5,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv("data/cleaned_data.csv")
 
+df = df.drop(columns=[
+    "Credit_Worthiness",
+    "credit_type",
+    "co-applicant_credit_type",
+    "submission_of_application",
+    "lump_sum_payment",
+    "Neg_ammortization",
+    "Interest_rate_spread",
+    "Upfront_charges",
+    "rate_of_interest"
+])
 features = df.drop("Status", axis=1)
 labels = df["Status"]
 
@@ -33,3 +46,26 @@ log_model.fit(features_train_scaled, labels_train)
 labels_pred = log_model.predict(features_test_scaled)
 
 print(accuracy_score(labels_test, labels_pred))
+print(confusion_matrix(labels_test, labels_pred))
+print(classification_report(labels_test, labels_pred))
+
+
+
+print("="*20)
+print("\nRandom Forest:")
+
+rf_model = RandomForestClassifier(
+    n_estimators=200,
+    max_depth=12,
+    min_samples_leaf=10,
+    random_state=42,
+    n_jobs=-1
+)
+
+rf_model.fit(features_train, labels_train)
+rf_pred = rf_model.predict(features_test)
+
+
+print(accuracy_score(labels_test, rf_pred))
+print(confusion_matrix(labels_test, rf_pred))
+print(classification_report(labels_test, rf_pred))
